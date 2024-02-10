bfed1b8b29ddaa32e1a64765c210fa2cb9078cf9
Gabriel <gabriel.d.albu@gmail.com>
2024-02-10 14:37:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import HistoryPage from './pages/History';
+import TeamsPage from './pages/PlayerTeams';
+import GetBetterPage from './pages/GetBetter';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/history" element={<HistoryPage />} />
+          <Route path="/teams" element={<TeamsPage />} />
+          <Route path="/getbetter" element={<GetBetterPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
