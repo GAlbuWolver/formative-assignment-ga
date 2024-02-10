81acf2c197e154332b40ba1a359af3577d04ae94
Gabriel <gabriel.d.albu@gmail.com>
2024-02-10 16:48:47 -0500

    api stuff

--- src/pages/PlayerTeams.js
+++ src/pages/PlayerTeams.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function History() {
+function PlayerTeams() {
+  const [teamName, setTeamName] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://v1.basketball.api-sports.io/leagues', {
+          headers: {
+            'x-rapidapi-key': '11848bde7dac1824600a0e7dae4c93d1',
+            'x-rapidapi-host': 'v1.basketball.api-sports.io'
+          }
+        });
+        
+        //console.log(response.data.response[0].name);
+        setTeamName(response.data.response[0].name);       
+      } catch (error) {
+        console.error('There was an error!', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>PlayerTeams Page</h1>
+    <div className='row'>
+      <h1>{teamName ? `Team: ${teamName}` : 'Loading team name...'}</h1>
     </div>
   );
 }
 
-export default History;
+export default PlayerTeams;
